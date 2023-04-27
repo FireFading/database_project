3a1b39fc1052465db11390e89820f78ff852f5b4
FireFading <svetl.rudnewa2014@yandex.ru>
2023-04-27 11:08:57 +0300

    refactor

--- scrypts/triggers.sql
+++ scrypts/triggers.sql
@@ -1,6 +1,7 @@
 USE chemistry;
 
 -- update amount of chemical substance when add new used substance record
+-- обновление количества версии химического вещества после добавления записи в таблицу использованных веществ
 CREATE TRIGGER update_chemical_version_amount AFTER INSERT ON UsedSubstance FOR EACH ROW BEGIN DECLARE new_amount DECIMAL(10, 6);
 
 SET
@@ -22,6 +23,7 @@ WHERE
 END;
 
 -- prevent adding UsedSubstance instance if laborant is under 21 years old
+-- предотвращение выдачи вещества, если лаборант моложе 21 года
 CREATE TRIGGER prevent_underage_laboratory_assistant BEFORE INSERT ON UsedSubstance FOR EACH ROW BEGIN DECLARE laborant_age INT;
 
 SELECT
@@ -40,6 +42,7 @@ END IF;
 END;
 
 -- check the danger class of the ChemicalSubstance before inserting a new UsedSubstance record
+-- предотвращение выдачи вещества с очень высокой степенью токсичности
 CREATE TRIGGER prevent_highly_toxic_chemical_substance BEFORE INSERT ON UsedSubstance FOR EACH ROW BEGIN DECLARE danger_class_name VARCHAR(45);
 
 SELECT
@@ -60,18 +63,20 @@ END IF;
 END;
 
 -- check expiration date before create UsedSubstance record
-CREATE TRIGGER check_chemical_version_expiration
-BEFORE INSERT ON chemistry.UsedSubstance
-FOR EACH ROW
-BEGIN
-    DECLARE version_expiration DATETIME;
+-- проверка срока годности вещества перед выдачей в использование
+CREATE TRIGGER check_chemical_version_expiration BEFORE INSERT ON chemistry.UsedSubstance FOR EACH ROW BEGIN DECLARE version_expiration DATETIME;
 
-    SELECT expiration_time INTO version_expiration
-    FROM chemistry.ChemicalVersion
-    WHERE id = NEW.chemical_version_id;
+SELECT
+    expiration_time INTO version_expiration
+FROM
+    chemistry.ChemicalVersion
+WHERE
+    id = NEW.chemical_version_id;
 
-    IF version_expiration < NOW() THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Cannot add UsedSubstance instance. The ChemicalVersion has expired.';
-    END IF;
+IF version_expiration < NOW () THEN SIGNAL SQLSTATE '45000'
+SET
+    MESSAGE_TEXT = 'Cannot add UsedSubstance instance. The ChemicalVersion has expired.';
+
+END IF;
+
 END;
